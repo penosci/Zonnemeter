71f6ccd644b70783cece29c6b9c1b1fff1948038
Unknown <stevenwoudstra@gmail.com>
2018-05-29 11:58:06 +0200

    added arduino code for simulating pic

---
+++ arduino/dummy_PIC/dummy_PIC.ino
@@ -0,0 +1,14 @@
+
+String output;
+void setup() {
+  Serial.begin(2400);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  String var1 = String(random(2000));
+  String var2 = String(random(1999));
+  output = String(var2+';'+var2);
+  Serial.println(output);
+  delay(1000);
+}
