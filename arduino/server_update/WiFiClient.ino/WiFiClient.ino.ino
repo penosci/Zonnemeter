64f5ca6ee577de9d2ebace5edce852a90acf9e30
Unknown <stevenwoudstra@gmail.com>
2018-06-05 10:54:51 +0200

    updateje

--- arduino/server_update/WiFiClient.ino/WiFiClient.ino.ino
+++ arduino/server_update/WiFiClient.ino/WiFiClient.ino.ino
@@ -1,5 +1,7 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+String incoming;
+String dataToSend;
 
 void setup() {
  
@@ -13,32 +15,51 @@ void setup() {
  
   }
   Serial.println("Connection made");
+  
  
 }
 
 void loop() {
  
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
- 
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://10.42.0.1:80/webserver/esp_update.php");      //Specify request destination
-   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-   //http.writeToStream(&Serial);
- 
-   int httpCode = http.POST("power=124&temp=21");   //Send the request
-   String payload = http.getString();                  //Get the response payload
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
+ while(Serial.available()) {
+
+  incoming= Serial.readString();// read the incoming data as string
+
+  Serial.println(incoming);
+  dataToSend = incoming;
+
+}
+ if(dataToSend!=""){
+  dataToSend = strtok_r(dataToSend, ";", &dataToSend)
+  send_to_server(dataToSend);
+  dataToSend="";
+  
  }
- 
-  delay(30000);  //Send a request every 30 seconds
+  
  
 }
+
+void send_to_server(String data) {
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+   
+     HTTPClient http;    //Declare object of class HTTPClient
+   
+     http.begin("http://10.42.0.1:80/webserver/esp_update.php");      //Specify request destination
+     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+     //http.writeToStream(&Serial);
+   
+     int httpCode = http.POST(data);   //Send the request
+     String payload = http.getString();                  //Get the response payload
+     Serial.println(httpCode);   //Print HTTP return code
+     Serial.println(payload);    //Print request response payload
+   
+     http.end();  //Close connection
+   
+   }else{
+   
+      Serial.println("Error in WiFi connection");   
+   
+  }
+}
+
+
